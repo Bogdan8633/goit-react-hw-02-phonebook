3a9ed7074e3bd345cae8bf194f45489b7a1a5650
Bogdan <bogdanberezka@gmail.com>
2023-01-24 20:24:16 +0200

    add-styles-to-filter

--- src/components/ContactBook/Filter/Filter.jsx
+++ src/components/ContactBook/Filter/Filter.jsx
@@ -1,11 +1,14 @@
-import styles from './filter.module.css';
 import PropTypes from 'prop-types';
 
+import styles from './filter.module.css';
+
 const Filter = ({ value, onChange }) => (
-  <label className={styles.label}>
-    Find contacts by name
-    <input type="text" value={value} onChange={onChange} />
-  </label>
+  <div className={styles.wrapper}>
+    <label className={styles.label} htmlFor="filter">
+      Find contacts by name
+    </label>
+    <input type="text" name="filter" value={value} onChange={onChange} />
+  </div>
 );
 
 export default Filter;
